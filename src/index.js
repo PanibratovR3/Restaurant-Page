22cdbb1b1bc799c0d0b012392dc42a52c5a0dd97
PanibratovR3 <roman.panibratov@gmail.com>
2025-05-23 13:09:15 +0300

    Added handlers for buttons on page: loading content. Changed color of text.

--- src/index.js
+++ src/index.js
@@ -2,4 +2,31 @@ import { drawHomePage } from "./homePage.js";
 import { drawMenuPage } from "./menuPage.js";
 import { drawAboutPage } from "./aboutPage.js";
 
+const [homeButton, menuButton, aboutButton] =
+  document.querySelectorAll(".navigation");
+
+homeButton.classList.add("selected");
+homeButton.addEventListener("click", () => {
+  homeButton.classList.add("selected");
+  menuButton.classList.remove("selected");
+  aboutButton.classList.remove("selected");
+
+  drawHomePage();
+});
+
+menuButton.addEventListener("click", () => {
+  homeButton.classList.remove("selected");
+  menuButton.classList.add("selected");
+  aboutButton.classList.remove("selected");
+
+  drawMenuPage();
+});
+
+aboutButton.addEventListener("click", () => {
+  homeButton.classList.remove("selected");
+  menuButton.classList.remove("selected");
+  aboutButton.classList.add("selected");
+
+  drawAboutPage();
+});
 drawHomePage();
